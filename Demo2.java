4c4ceb584dc83f0746a8504aac0b442c23ca46b4
yimin.zhou <95442563+yiminzhou347@users.noreply.github.com>
2023-03-22 14:35:39 -0400

    Delete Demo2.java

--- Demo2.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Demo2 {
-    public static void main(String[] args) {
-        System.out.println("This is Demo2.class file");
-    }
-}
-
-//C:\Users\yimin\Downloads\jenkins\src\main\java\org\example\Demo2.java
