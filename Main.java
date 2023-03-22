58f2a75483c702a7ea1b58932d1aa392b62a3cec
yimin.zhou <95442563+yiminzhou347@users.noreply.github.com>
2023-03-22 13:24:50 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        test1.print("Hello jenkins");
+    }
+}
---
+++ test1.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class test1 {
+    public static void print(String s ){
+        System.out.println(s );
+    }
+}
